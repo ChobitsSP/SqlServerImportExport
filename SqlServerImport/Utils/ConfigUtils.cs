0b7397640dca72ec79c299f40b8c738ea59c755a
Chao <zhangchao@hsort.com>
2025-05-23 18:16:02 +0800

    初始化

---
+++ SqlServerImport/Utils/ConfigUtils.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace SqlServerImport
+{
+    public static class ConfigUtils
+    {
+        public static IConfigurationRoot GetConfig()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+#if DEBUG
+                    .AddJsonFile("appsettings.Development.json")
+#else
+                    .AddJsonFile("appsettings.json")
+#endif
+                    .Build();
+
+            return configuration;
+        }
+
+        /// <summary>
+        /// 获取数据库连接字符串
+        /// </summary>
+        public static string GetConnectionString(string name = "Default")
+        {
+            var key = "ConnectionStrings" + name;
+            var constr = Environment.GetEnvironmentVariable(key);
+            if (!string.IsNullOrEmpty(constr)) return constr;
+            constr = GetConfig().GetConnectionString(name);
+            return constr;
+        }
+
+        public static string GetSectionValue(string name, string defVal = null)
+        {
+            return GetConfig().GetSectionValue(name, defVal);
+        }
+
+        public static int? GetInt(string name)
+        {
+            var str = GetSectionValue(name);
+            if (string.IsNullOrEmpty(str)) return null;
+            return int.TryParse(str, out var result) ? result : null;
+        }
+
+        public static string GetSectionValue(this IConfiguration config, string name, string defVal = null)
+        {
+            return config.GetSection(name)?.Value ?? defVal;
+        }
+    }
+}
